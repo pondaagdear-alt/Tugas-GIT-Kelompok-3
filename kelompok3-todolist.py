57828c22b87f049bf4e1c5db3ad3d1574418b752
briankindangen2006-cmyk <briankindangen2006@gmail.com>
2025-11-26 14:06:20 +0800

    Update kelompok3-todolist.py
    Menambahkan Fungsi Edit dan Hapus Tugas

--- kelompok3-todolist.py
+++ kelompok3-todolist.py
@@ -1,1 +1,17 @@
-tes
+# Fungsi Edit dan Hapus Tugas
+
+def simpan_data():
+    with open("to_do.txt", "w") as file:
+        for tugas in to_do_list:
+            file.write(tugas + "\n")
+    print("✔ Data berhasil disimpan ke to_do.txt!\n")
+
+
+def muat_data():
+    try:
+        with open("to_do.txt", "r") as file:
+            for baris in file:
+                to_do_list.append(baris.strip())
+        print("✔ Data berhasil dimuat!\n")
+    except FileNotFoundError:
+        print("ℹ Tidak ditemukan file sebelumnya. Membuat baru...\n")
