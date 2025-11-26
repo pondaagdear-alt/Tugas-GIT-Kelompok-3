cab638132e295be695a5a7518c9c32b9a0e4a63b
briankindangen2006-cmyk <briankindangen2006@gmail.com>
2025-11-26 14:23:39 +0800

    Update kelompok3-todolist.py
    Menambahkan Fungsi Simpan Dan Muat Data

--- kelompok3-todolist.py
+++ kelompok3-todolist.py
@@ -50,3 +50,21 @@ def hapus_tugas():
     except ValueError:
         print("✖ Input harus berupa angka.\n")
 
+# Fungsi Simpan dan Muat Data
+
+def simpan_data():
+    with open("to_do.txt", "w") as file:
+        for tugas in to_do_list:
+            file.write(tugas + "\n")
+    print("✔ Data berhasil disimpan ke to_do.txt!\n")
+
+
+def muat_data():
+    try:
+        with open("to_do.txt", "r") as file:
+            for baris in file:
+                to_do_list.append(baris.strip())
+        print("✔ Data berhasil dimuat!\n")
+    except FileNotFoundError:
+        print("ℹ Tidak ditemukan file sebelumnya. Membuat baru...\n")
+
